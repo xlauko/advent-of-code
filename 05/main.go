3fbabfbca42b5d7ba39d9fb5ec932d491b8aceb3
Henrich Lauko <xlauko@mail.muni.cz>
2020-12-06 18:30:11 +0100

    05: add function utils

--- 05/main.go
+++ 05/main.go
@@ -8,23 +8,20 @@ import (
 )
 
 func main() {
-	var seen [1024]int
-	min, max := len(seen), 0
+	seen := make([]int, 1024)
+	max := 0
 	utils.ScanLine("big.txt", func(line string) {
 		replacer := strings.NewReplacer("F", "0", "B", "1", "L", "0", "R", "1")
 		id, _ := strconv.ParseInt(replacer.Replace(line), 2, 0)
-
-		if int(id) < min {
-			min = int(id)
-		}
+		seen[id] = 1
 
 		if int(id) > max {
 			max = int(id)
 		}
-		seen[id] = 1
 	})
 
-	place, _ := utils.FindIf(seen[min:max], func(val int) bool { return val == 0 })
+	min, _ := utils.FindIf(seen, utils.IsOne)
+	place, _ := utils.FindIf(seen[min:max], utils.IsZero)
 
 	fmt.Println("Part 1: ", max)
 	fmt.Println("Part 2: ", place+min)
---
+++ utils/functions.go
@@ -0,0 +1,11 @@
+package utils
+
+// IsOne predicate
+func IsOne(val int) bool {
+	return val == 1
+}
+
+// IsZero predicate
+func IsZero(val int) bool {
+	return val == 0
+}
