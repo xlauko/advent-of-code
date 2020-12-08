d75be9960d0fc4614fa2f695dbb40080f0db22d0
Henrich Lauko <xlauko@mail.muni.cz>
2020-12-08 16:33:27 +0100

    08: create more reusable code

--- 08/main.go
+++ 08/main.go
@@ -7,72 +7,99 @@ import (
 	"utils/utils"
 )
 
-// Op operation
-type Op struct {
-	name string
-	off  int
+type inst struct {
+	op   string
+	args []int
 }
 
-func run(code []Op) (bool, int) {
-	acc := 0
-	ip := 0
-	seen := make(map[int]bool)
+type machine struct {
+	code []inst
+	seen map[int]bool
+	pc   int
+	acc  int
+}
 
-	for {
-		if ip == len(code) {
-			return true, acc
-		}
+func parse(line string) inst {
+	split := strings.Split(line, " ")
+	arg, _ := strconv.Atoi(split[1])
+	return inst{op: split[0], args: []int{arg}}
+}
 
-		op := code[ip]
-		if seen[ip] {
-			return false, acc
-		}
+func load(path string) []inst {
+	var code []inst
+	utils.ScanLine(path, func(line string) {
+		code = append(code, parse(line))
+	})
+	return code
+}
 
-		seen[ip] = true
-		switch op.name {
-		case "acc":
-			acc += op.off
-			ip++
-			break
-		case "jmp":
-			ip += op.off
-			break
-		case "nop":
-			ip++
-			break
+func (m machine) terminated() bool {
+	return m.pc == len(m.code)
+}
+
+func (m machine) loops() bool {
+	return m.seen[m.pc]
+}
+
+func (m *machine) interpret(i inst) int {
+	m.seen[m.pc] = true
+	switch i.op {
+	case "acc":
+		m.acc += i.args[0]
+		return 1
+	case "jmp":
+		return i.args[0]
+	case "nop":
+		return 1
+	}
+	panic("unknown operation " + i.op)
+}
+
+func (m machine) current() inst {
+	return m.code[m.pc]
+}
+
+func (m *machine) run() (bool, int) {
+	for !m.terminated() {
+		if m.loops() {
+			return false, m.acc
 		}
+		m.pc += m.interpret(m.current())
+	}
+	return true, m.acc
+}
+
+func eval(code []inst) (bool, int) {
+	var m machine
+	m.seen = make(map[int]bool)
+	m.code = code
+	return m.run()
+}
+
+func (i *inst) flip() {
+	switch i.op {
+	case "jmp":
+		i.op = "nop"
+	case "nop":
+		i.op = "jmp"
 	}
 }
 
-func flip(code []Op, i int) {
-	if code[i].name == "jmp" {
-		code[i].name = "nop"
-	} else if code[i].name == "nop" {
-		code[i].name = "jmp"
-	}
-}
-
-func terminates(code []Op) int {
-	for i := 0; i < len(code); i++ {
-		flip(code, i)
-		fix, acc := run(code)
-		if fix {
-			return acc
+func debug(code []inst) int {
+	for i := range code {
+		code[i].flip()
+		halts, res := eval(code)
+		if halts {
+			return res
 		}
-		flip(code, i)
+		code[i].flip()
 	}
-	return 0
+	panic("does not terminate")
 }
 
 func main() {
-	var code []Op
-	utils.ScanLine("big.txt", func(line string) {
-		split := strings.Split(line, " ")
-		off, _ := strconv.Atoi(split[1])
-		op := Op{name: split[0], off: off}
-		code = append(code, op)
-	})
-	_, acc := run(code)
-	fmt.Println("Part 1: ", acc)
-	fmt.Println("Part 2: ", terminates(code))
+	code := load("big.txt")
+	_, res := eval(code)
+	fmt.Println("Part 1: ", res)
+	fmt.Println("Part 2: ", debug(code))
 }
