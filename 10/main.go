d5195de75a626a8dac092ee20e4bf7375112ad05
Henrich Lauko <xlauko@mail.muni.cz>
2020-12-10 07:27:24 +0100

    10: simplify

--- 10/main.go
+++ 10/main.go
@@ -9,7 +9,6 @@ import (
 
 func arrangements(adaps []int) int {
 	tab := map[int]int{0: 1}
-	adaps = append(adaps, adaps[len(adaps)-1]+3)
 	for _, adap := range adaps {
 		if val, ok := tab[adap-1]; ok {
 			tab[adap] += val
@@ -25,20 +24,20 @@ func arrangements(adaps []int) int {
 }
 
 func main() {
-	var adapters []int
-	adapters = append(adapters, 0)
+	adaps := []int{0}
 	utils.ScanLine("big.txt", func(line string) {
 		a, _ := strconv.Atoi(line)
-		adapters = append(adapters, a)
+		adaps = append(adaps, a)
 	})
+	adaps = append(adaps, adaps[len(adaps)-1]+3)
 
-	sort.Ints(adapters)
+	sort.Ints(adaps)
 	jolts := make(map[int]int)
-	for i := 1; i < len(adapters); i++ {
-		diff := adapters[i] - adapters[i-1]
+	for i := 1; i < len(adaps); i++ {
+		diff := adaps[i] - adaps[i-1]
 		jolts[diff]++
 	}
 
 	fmt.Println("Part 1: ", jolts[1]*(jolts[3]+1))
-	fmt.Println("Part 2: ", arrangements(adapters))
+	fmt.Println("Part 2: ", arrangements(adaps))
 }
