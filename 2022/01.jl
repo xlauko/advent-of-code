57d59ea2ac009cb1ac9dcd12d1779274e2fd26e8
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-12-01 10:01:15 +0100

    2022: fix 01

--- 2022/01.jl
+++ 2022/01.jl
@@ -1,7 +1,7 @@
 using Pipe
 
 int(v) = parse(Int, v)
-const input = (@pipe split(read(stdin, String), "\n\n") .|> split |> map.(int, _) ) .|> sum
+const input = (@pipe split(read(stdin, String), "\n\n") .|> split .|> map(int, _) ) .|> sum
 
 println("Part 1: ", maximum(input))
 println("Part 2: ", sum(partialsort(input, 1:3, rev=true)))
