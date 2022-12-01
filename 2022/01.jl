113069e7de8dad70dfa0033f09116924060dce82
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-12-01 09:58:04 +0100

    2022: update 01

--- 2022/01.jl
+++ 2022/01.jl
@@ -1,11 +1,7 @@
 using Pipe
 
-const input = @pipe (ARGS[1]
-    |> read(_, String) |> strip |> split(_, "\n\n")
-    |> split.(_, "\n")
-    |> map.(x -> parse(Int, x), _)
-    |> sum.(_)
-)
+int(v) = parse(Int, v)
+const input = (@pipe split(read(stdin, String), "\n\n") .|> split |> map.(int, _) ) .|> sum
 
 println("Part 1: ", maximum(input))
 println("Part 2: ", sum(partialsort(input, 1:3, rev=true)))
