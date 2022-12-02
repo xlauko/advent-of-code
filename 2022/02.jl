7bfd85f40522c521153cf46764a022cd89c3ca02
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-12-02 08:02:38 +0100

    2022: update 02

--- 2022/02.jl
+++ 2022/02.jl
@@ -1,12 +1,12 @@
 using Mods
 
-const input = readlines(stdin) .|> split
-
 parse_one(c, base) = Mod{3}(c[1] - base)
 parse(p) = parse_one(p[1], 'A'), parse_one(p[2], 'X')
 
+const input = readlines(stdin) .|> split .|> parse
+
 play(a, b) = value(b - a + 1) * 3 + value(b) + 1
-solve(plays, convert) = sum((plays .|> parse) .|> p -> play(p[1], convert(p[1], p[2])))
+solve(plays, convert) = sum(plays .|> p -> play(p[1], convert(p[1], p[2])))
 
 println("Part 1: ", solve(input, (a, x) -> x))
 println("Part 2: ", solve(input, (a, x) -> a + 2 + x))
