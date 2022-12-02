1f340b98c4e4b30efc3c1c1eee19612ec62de7eb
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-12-02 07:38:20 +0100

    2022: update 02

--- 2022/02.jl
+++ 2022/02.jl
@@ -1,26 +1,19 @@
+using Mods
+
 const input = readlines(stdin) .|> split
 
-value(c, base) = Int(c[1]) - Int(base)
+parse(c, base) = Mod{3}(Int(c[1]) - Int(base))
 
-function score(a, b)
-    if a == b
-        return 3
-    elseif ((a + 1) % 3) == b
-        return 6
-    end
-    return 0
-end
-
-play(a, b) = score(a, b) + b + 1
+play(a, b) = value(b - a + 1) * 3 + value(b) + 1
 
 function solve(plays, convert)
     s = 0
     for p in plays
-        a, b = value(p[1], 'A'), value(p[2], 'X')
+        a, b = parse(p[1], 'A'), parse(p[2], 'X')
         s += play(a, convert(a, b))
     end
     return s
 end
 
 println("Part 1: ", solve(input, (a, x) -> x))
-println("Part 2: ", solve(input, (a, x) -> (a + 2 + x) % 3))
+println("Part 2: ", solve(input, (a, x) -> a + 2 + x))
