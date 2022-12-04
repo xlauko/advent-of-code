b20452bfebc6dce1e422f6ffd78e1aa205d0df3f
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-12-04 06:59:34 +0100

    2022: fix 04

--- 2022/04.jl
+++ 2022/04.jl
@@ -1,8 +1,10 @@
 using  DelimitedFiles
 
+int = v -> parse(Int, v)
+
 const input = (readlines(stdin)
     .|> l -> match(r"(\d+)-(\d+),(\d+)-(\d+)", l)
-     |> m -> ((m[1], m[2]), (m[3], m[4]))
+     |> m -> ((int(m[1]), int(m[2])), (int(m[3]), int(m[4])))
 )
 
 contains(a, b) = a[1] ≤ b[1] ≤ b[2] ≤ a[2]
