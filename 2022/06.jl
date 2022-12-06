4c2c1de19b94348130df3d3956ad4a8bb2ddb538
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-12-06 07:05:54 +0100

    2022: fix 06

--- 2022/06.jl
+++ 2022/06.jl
@@ -1,7 +1,7 @@
 const input = read(stdin, String)
 
 function solve(data, len)
-    for i in len:length(data)
+    for i in 1:length(data)
         part = Set(data[i:i + len - 1])
         if length(part) == len
             return i + len - 1
