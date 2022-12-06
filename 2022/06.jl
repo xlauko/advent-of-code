64f19ee426ad11edb6ce791041c11b388f016edf
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-12-06 06:21:31 +0100

    2022: add 06

---
+++ 2022/06.jl
@@ -0,0 +1,14 @@
+const input = read(stdin, String)
+
+function solve(data, len)
+    for i in len:length(data)
+        part = Set(data[i:i + len - 1])
+        if length(part) == len
+            return i + len - 1
+        end
+    end
+    return 0
+end
+
+println("Parst 1: ", solve(input, 4))
+println("Parst 2: ", solve(input, 14))
