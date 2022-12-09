9336f550cc8edf5da4d7da0b6e6e3316a770b13e
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-12-09 08:25:16 +0100

    2022: simplify 09

--- 2022/09.jl
+++ 2022/09.jl
@@ -1,37 +1,33 @@
 int(s) = parse(Int, s)
+pairwise(x) = zip(x, last(Iterators.peel(x)))
 
 const data = readlines(stdin) .|> split
 
 const dirs = Dict("U" => (0, 1), "D" => (0, -1), "R" => (1, 0), "L" => (-1, 0))
 
-function update(h, t)
+function update((h, t))
     Δ = h .- t
-    if maximum(abs.(Δ)) > 1 || sum(abs.(Δ)) > 2
+    if maximum(abs.(Δ)) > 1
         return t .+ clamp.(Δ, -1, 1)
     end
     return t
 end
 
 function solve(cmds, len)
-    h = (0, 0)
-    t = fill((0, 0), len)
+    r = fill((0, 0), len)
 
     pos = Set()
-    push!(pos, last(t))
+    push!(pos, last(r))
 
     for cmd in cmds
         for i in 1:int(cmd[2])
-            h = h .+ dirs[cmd[1]]
-            t[1] = update(h, t[1])
-            for i in 2:len
-                t[i] = update(t[i - 1], t[i])
-            end
-            push!(pos, last(t))
+            r = [r[1] .+ dirs[cmd[1]]; pairwise(r) .|> update]
+            push!(pos, last(r))
         end
     end
 
-    return length(pos)
+    return length(pos) + 1
 end
 
-println("Part 1: ", solve(data, 1))
-println("Part 2: ", solve(data, 9))
+println("Part 1: ", solve(data, 2))
+println("Part 2: ", solve(data, 10))
