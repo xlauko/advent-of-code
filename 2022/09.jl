b98e64e1dbf2aaf0ce3fd6bb7b4a206b3901e148
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-12-09 09:52:21 +0100

    2022: clean up 09

--- 2022/09.jl
+++ 2022/09.jl
@@ -1,11 +1,9 @@
 using Distances
-using Base.Iterators
 
 int(s) = parse(Int, s)
 pairwise(x) = zip(x, x[2:end])
 
 const data = readlines(stdin) .|> split
-
 const dirs = Dict("U" => [0, 1], "D" => [0, -1], "R" => [1, 0], "L" => [-1, 0])
 
 update((h, t)) = chebyshev(h, t) > 1 ? t + sign.(h - t) : t
@@ -16,11 +14,9 @@ function solve(cmds, len)
     pos = Set()
     push!(pos, last(r))
 
-    for cmd in cmds
-        for _ in 1:int(cmd[2])
-            r = [[r[1] + dirs[cmd[1]]]; pairwise(r) .|> update]
-            push!(pos, last(r))
-        end
+    for (dir, step) in cmds, _ in 1:int(step)
+        r = [[r[1] + dirs[dir]]; pairwise(r) .|> update]
+        push!(pos, last(r))
     end
 
     return length(pos)
