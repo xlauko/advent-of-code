c641d6111ec41ea0385892bf4c2cbe868466ba36
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-12-10 07:52:14 +0100

    2022: update 10

--- 2022/10.jl
+++ 2022/10.jl
@@ -7,12 +7,11 @@ const input = readlines(stdin) .|> split
 pixel(T, X) = ((T - 1) % 40) in X - 1:X + 1 ? '#' : ' '
 
 function solve(cmds, cycles)
-    PC = 1; X = 1; M = nothing
-    result = 0
+    PC = 1; X = 1; M = nothing; ACC = 0
 
     for T in 1:cycles + 1
         print(pixel(T, X), ((T % 40) == 0 ? "\n" : ""))
-        if (T % 40 == 20) result += T * X end
+        if (T % 40 == 20) ACC += T * X end
 
         if M !== nothing
             X, M = M + X, nothing
@@ -25,7 +24,7 @@ function solve(cmds, cycles)
             PC += 1
         end
     end
-    return result
+    return ACC
 end
 
 println("\nPart 1: ", solve(input, 220))
