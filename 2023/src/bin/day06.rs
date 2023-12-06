f429a5771bdd34a74a47b4da5cb679c32ae7f61d
Henrich Lauko <henrich.lauko@trailofbits.com>
2023-12-06 06:37:40 +0100

    2023: Add day 6.

---
+++ 2023/src/bin/day06.rs
@@ -0,0 +1,20 @@
+use aoc::{input::{file_read_lines, nums}, iter::IteratorExt};
+
+fn solve(time: Vec<u64>, distance: Vec<u64>) -> u64 {
+    time.iter()
+        .zip(distance.iter())
+        .map(|(&t, &d)| {
+            (0..t).filter(|&h| h * (t - h) > d).count() as u64
+        })
+        .product()
+}
+
+fn main() {
+    let lines = file_read_lines("input.txt");
+    let parse = |l: &str| nums(l.split(":").nthu(1));
+
+    println!("Part 1: {}", solve(parse(&lines[0]), parse(&lines[1])));
+
+    let kern = |line: &str| parse(&line.replace(" ", ""));
+    println!("Part 2: {}", solve(kern(&lines[0]), kern(&lines[1])));
+}
