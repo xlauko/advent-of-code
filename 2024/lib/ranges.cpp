b8977e9ef6331aea59b95f6196593df916e2ff76
xlauko <xlauko@mail.muni.cz>
2024-12-16 22:27:04 +0100

    2024: Clean up split adaptor

--- 2024/lib/ranges.cpp
+++ 2024/lib/ranges.cpp
@@ -2,25 +2,24 @@ export module aoc:ranges;
 
 import std;
 
+import :parse;
+
 export namespace aoc::views {
 
-    struct split_to_string_views_fn {
+    struct split_to_string_views_fn : std::ranges::range_adaptor_closure< split_to_string_views_fn > {
         const char delimiter;
+
+        explicit split_to_string_views_fn(char delimiter) noexcept : delimiter(delimiter) {}
 
         constexpr auto operator()(std::string_view input) const noexcept {
             return input | std::views::split(delimiter) | std::views::transform([](auto&& subrange) noexcept {
                 return std::string_view(&*subrange.begin(), subrange.size());
             });
         }
-
-        template<std::ranges::range range_t>
-        friend constexpr auto operator|(range_t&& range, const split_to_string_views_fn& splitter) noexcept {
-            return splitter(std::forward<range_t>(range));
-        }
     };
 
     constexpr auto split_to_string_views(char delimiter) noexcept {
-       return split_to_string_views_fn{delimiter};
+       return split_to_string_views_fn(delimiter);
     }
 
 } // namespace aoc::views
